4de89f0441bde4f1c5b59b65d3e41f2b7a4322aa
Akbar Dwi Riyanto <akbardwi500@gmail.com>
2023-03-29 17:31:26 +0700

    update

--- app/src/main/java/com/si61/payobejalan/MainActivity.java
+++ app/src/main/java/com/si61/payobejalan/MainActivity.java
@@ -1,20 +1,26 @@
 package com.si61.payobejalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton fabTambah;
     private RecyclerView rvdestinasi;
     private MyDatabaseHelper myDB;
+    private ArrayList<String> arrName, arrAlamat, arrJam;
+    private AdapterDestinasi adDestinasi;
 
 
     @Override
@@ -36,9 +42,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void SQLiteToArrayList(){
+        Cursor varCursor = myDB.bacaDataDestinasi();
+        if(varCursor.getCount() == 0){
+            Toast.makeText(this, "Data Tidak Tersedia!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            while (varCursor.moveToNext()){
+                arrName.add(varCursor.getString(1));
+                arrAlamat.add(varCursor.getString(2));
+                arrJam.add(varCursor.getString(3));
+
+            }
+        }
 
     }
     private void tampilDestinasi(){
+        arrName = new ArrayList<>();
+        arrAlamat = new ArrayList<>();
+        arrJam = new ArrayList<>();
 
+        SQLiteToArrayList();
+
+        adDestinasi = new AdapterDestinasi(MainActivity.this, arrName, arrAlamat, arrJam);
+        rvdestinasi.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rvdestinasi.setAdapter(adDestinasi);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilDestinasi();
     }
 }
--- app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
+++ app/src/main/java/com/si61/payobejalan/MyDatabaseHelper.java
@@ -2,6 +2,7 @@ package com.si61.payobejalan;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -60,5 +61,18 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 
         long eksekusi = db.insert(TABLE_NAME, null, cv);
         return eksekusi;
+    }
+
+    public Cursor bacaDataDestinasi(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT * FROM " + TABLE_NAME;
+
+        Cursor varCursor = null;
+        if(db!=null){
+            varCursor = db.rawQuery(query, null);
+        }
+
+        return varCursor;
+
     }
 }
